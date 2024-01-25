ebcf93937e790179d9dec4d3e6361457eb8ecc6d
kferi <kissferenc1988@hotmail.com>
2024-01-25 13:12:48 +0100

    type hinting

--- milestone_2/utils/database.py
+++ milestone_2/utils/database.py
@@ -1,21 +1,20 @@
+from ast import TypeAlias
+from typing import List, Dict, Union
 from .database_connect import DatabaseConnection
 
-# def create_book_table(conn):
-#     cursor = conn.cursor()
-#     cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
-#     conn.commit()
+Book: TypeAlias = Dict(str, Union(str,int))
 class Bookshelf:
     def __init__(self):
         with DatabaseConnection('data.db') as connection:
             cursor = connection.cursor()
             cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
-    def add_book(self, name, author, r):
+    def add_book(self, name: str, author: str, r: str) -> None:
         with DatabaseConnection('data.db') as connection:
             cursor = connection.cursor()
             cursor.execute('INSERT INTO books VALUES(?, ?, ?)', (name, author, True if r=='y' else False))
 
-    def mark_read(self, search):
+    def mark_read(self, search: str) -> int:
         with DatabaseConnection('data.db') as connection:
             cursor = connection.cursor()
             cursor.execute('SELECT read FROM books WHERE name = ?', (search,))
@@ -24,14 +23,14 @@ class Bookshelf:
             rc = cursor.rowcount
         return rc
     
-    def get_all_books(self):
+    def get_all_books(self) -> List[Book]:#List[Dict(str, Union(str,int))]:
         with DatabaseConnection('data.db') as connection:
             cursor = connection.cursor()
             cursor.execute('SELECT * FROM books')
             books = [{'name': row[0], 'author': row[1], 'read': row[2]} for row in cursor.fetchall()]
         return books
 
-    def delete_book(self, search):
+    def delete_book(self, search: str) -> int:
         with DatabaseConnection('data.db') as connection:
             cursor = connection.cursor()
             cursor.execute('DELETE FROM BOOKS where name = ?', (search,))
