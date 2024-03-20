18888332f3b097400fe7fd2c3b76f3ecabb86fba
kferi <kissferenc1988@hotmail.com>
2024-03-20 11:06:17 +0100

    chaging the bookscraper to async

--- milestone_4/app.py
+++ milestone_4/app.py
@@ -1,5 +1,9 @@
 import requests
 import logging
+import aiohttp
+import asyncio
+import async_timeout
+import time
 
 from pages.all_books import AllBooksPage
 
@@ -17,9 +21,38 @@ page = AllBooksPage(page_content)
 
 books = page.books
 
-for page_num in range(1, page.page_count):
-    url = f'https://books.toscrape.com/catalogue/page-{page_num + 1}.html'
-    page_content = requests.get(url).content
+#Async code:
+
+async def fetch_page(session, url):
+    page_start = time.time()
+    async with async_timeout.timeout(10):
+        async with session.get(url) as response:
+            print(f'Page took {time.time() - page_start}')
+            return await response.text()
+
+async def get_multiple_pages(*urls):
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for url in urls:
+            tasks.append(fetch_page(session, url))
+        grouped_tasks = asyncio.gather(*tasks)
+        return await grouped_tasks
+
+urls = [f'https://books.toscrape.com/catalogue/page-{page_num + 1}.html' for page_num in range(1, page.page_count)]
+start = time.time()
+
+pages = asyncio.run(get_multiple_pages(*urls))
+print(f'All took {time.time()-start}')
+
+for page_content in pages:
     logger.debug('Creating AllBooksPage from page content.')
     page = AllBooksPage(page_content)
     books.extend(page.books)
+
+# Original code
+# for page_num in range(1, page.page_count):
+#     url = f'https://books.toscrape.com/catalogue/page-{page_num + 1}.html'
+#     page_content = requests.get(url).content
+#     logger.debug('Creating AllBooksPage from page content.')
+#     page = AllBooksPage(page_content)
+#     books.extend(page.books)
