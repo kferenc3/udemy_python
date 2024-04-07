7b2eb53b806f6023f9a8a3eb4f9328686e4e97d4
kferi <kissferenc1988@hotmail.com>
2024-04-07 18:09:25 +0200

    tkinter

---
+++ gui-development/packing.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import ttk
+ 
+root = tk.Tk()
+root.title("Packing & Frames")
+
+main = ttk.Frame(root)
+main.pack(side="left", fill="both", expand=True)
+
+tk.Label(main, text="Label top", bg="red").pack(side="top", fill="both", expand=True)
+tk.Label(main, text="Label top", bg="red").pack(side="top", fill="both", expand=True)
+tk.Label(root, text="Label left", bg="green").pack(side="left", fill="both", expand=True)
+root.mainloop()
